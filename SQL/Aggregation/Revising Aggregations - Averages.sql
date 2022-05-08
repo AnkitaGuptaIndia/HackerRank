7ffa47b3621daf2b0d48b66cc8bce6c022ad9e33
Ankita Gupta <105074319+AnkitaGuptaIndia@users.noreply.github.com>
2022-05-08 09:32:39 +0530

    Create Revising Aggregations - Averages.sql

---
+++ SQL/Aggregation/Revising Aggregations - Averages.sql
@@ -0,0 +1,11 @@
+/*
+Query the average population of all cities in CITY where District is California.
+
+Input Format
+
+The CITY table is described as follows:
+*/
+
+SELECT avg(population)
+from city
+where district = 'California';
