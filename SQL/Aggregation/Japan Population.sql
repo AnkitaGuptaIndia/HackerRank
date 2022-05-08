767e27736db8bc659d3f90fd962f0bf8889c567a
Ankita Gupta <105074319+AnkitaGuptaIndia@users.noreply.github.com>
2022-05-08 09:37:03 +0530

    Create Japan Population.sql

---
+++ SQL/Aggregation/Japan Population.sql
@@ -0,0 +1,13 @@
+/*
+
+Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.
+
+Input Format
+
+The CITY table is described as follows:
+
+*/
+
+select sum(population)
+from city
+where countrycode = 'JPN';
